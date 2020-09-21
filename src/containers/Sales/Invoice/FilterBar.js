ce319ba07d337684abf3489e3f16e3304cabe337
ssemenidov <23sergey03@gmail.com>
2020-09-21 14:17:25 +0800

    filters sales,installations

--- src/containers/Sales/Invoice/FilterBar.js
+++ src/containers/Sales/Invoice/FilterBar.js
@@ -1,28 +1,65 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import FilterByParametersPartners from '../../../components/Filters/FilterByCompany/FilterByCompany';
+import React, { useState } from 'react';
 import { FilterMenu, SearchTitle, FilterText } from '../../../components/Styles/StyledFilters';
-import FilterByBIN from '../../../components/Filters/FilterByBIN/FilterByBIN';
-import { BtnGroup, ResetButton, SubmitButton } from '../../../components/Styles/ButtonStyles';
-import { getPartnersData, filterPartners } from '../../../store/actions/actions';
 
+import { BtnGroup, ResetButton, SubmitButton } from '../../../components/Styles/ButtonStyles';
+
+import { Collapse } from '@material-ui/core';
+import { FilterSectionTitle, FilterSection, FilterSectionTitleText } from '../../../components/Styles/StyledFilters';
+import ArrowUp from '../../../components/CollapseArrows/CollapseArrowIcons/ArrowUp';
+import ArrowDown from '../../../components/CollapseArrows/CollapseArrowIcons/ArrowDown';
+import InputAnchor from '../../../components/Inputs/InputAnchor';
+import SelectAnchor from '../../../components/Inputs/SelectAnchor';
 const FilterBar = () => {
-  const dispatch = useDispatch();
+  const [open1, setOpen1] = useState(true);
+  const [open2, setOpen2] = useState(true);
+  const [open3, setOpen3] = useState(true);
+
   return (
     <FilterMenu>
       <SearchTitle>
         <FilterText>Поиск</FilterText>
       </SearchTitle>
-      <FilterByParametersPartners />
+      <FilterSection>
+        <FilterSectionTitle onClick={() => setOpen1(!open1)}>
+          <FilterSectionTitleText> по дате</FilterSectionTitleText>
+          {open1 && <ArrowUp />}
+          {!open1 && <ArrowDown />}
+        </FilterSectionTitle>
+
+        <Collapse in={open1}>
+          <SelectAnchor options={[{ label: 'case 1' }, { label: 'case 2' }]} placeholder="Выберите период" />
+        </Collapse>
+      </FilterSection>
+      <FilterSection>
+        <FilterSectionTitle onClick={() => setOpen2(!open2)}>
+          <FilterSectionTitleText> по параметрам</FilterSectionTitleText>
+          {open2 && <ArrowUp />}
+          {!open2 && <ArrowDown />}
+        </FilterSectionTitle>
+
+        <Collapse in={open2}>
+          <SelectAnchor options={[{ label: 'case 1' }, { label: 'case 2' }]} placeholder="Код проекта" />
+          <SelectAnchor options={[{ label: 'case 1' }, { label: 'case 2' }]} placeholder="Номер приложения" />
+          <SelectAnchor options={[{ label: 'case 1' }, { label: 'case 2' }]} placeholder="Бренд" />
+          <SelectAnchor options={[{ label: 'case 1' }, { label: 'case 2' }]} placeholder="Рекламодатель" />
+          <SelectAnchor options={[{ label: 'case 1' }, { label: 'case 2' }]} placeholder="Рекламное агенство" />
+        </Collapse>
+      </FilterSection>
+      <FilterSection>
+        <FilterSectionTitle onClick={() => setOpen3(!open3)}>
+          <FilterSectionTitleText> По менеджерам</FilterSectionTitleText>
+          {open3 && <ArrowUp />}
+          {!open3 && <ArrowDown />}
+        </FilterSectionTitle>
+
+        <Collapse in={open3}>
+          <SelectAnchor options={[{ label: 'case 1' }, { label: 'case 2' }]} placeholder="Ответственный" />
+          <SelectAnchor options={[{ label: 'case 1' }, { label: 'case 2' }]} placeholder="По продажам" />
+        </Collapse>
+      </FilterSection>
       <BtnGroup>
-        <ResetButton
-          onClick={() => {
-            dispatch({ type: 'CLEAR_PARTNERS_FILTER' });
-            dispatch(getPartnersData());
-          }}>
-          Очистить
-        </ResetButton>
-        <SubmitButton onClick={() => dispatch(filterPartners())}>Искать</SubmitButton>
+        <ResetButton>Очистить</ResetButton>
+        <SubmitButton>Искать</SubmitButton>
       </BtnGroup>
     </FilterMenu>
   );
