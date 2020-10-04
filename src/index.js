05ef7f56aeb28d303169546ec129b97dbd7ecb55
ssemenidov <23sergey03@gmail.com>
2020-10-04 19:01:35 +0800

    +

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client
 
 const client = new ApolloClient({
   uri: 'https://allbot.online/api/',
+  fetchOptions: {
+    mode: 'no-cors',
+  },
   cache: new InMemoryCache(),
 });
 
