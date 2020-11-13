6da1d020a518f085aac7be60c28ee93809309b37
Doniyor Rajabov <donickuzb@gmail.com>
2020-11-13 20:58:40 +0500

    add icons for inputs

--- src/containers/Sales/Com_projects/FilterBar.js
+++ src/containers/Sales/Com_projects/FilterBar.js
@@ -1,14 +1,19 @@
 import React, { useState, useContext } from 'react';
 import { comProjectContext } from './Com_projects';
 
-import {
-  FilterMenu,
-  SearchTitle,
-  FilterText,
+// ICONS
+import date from '../../../img/left-bar/filter/date.svg';
+import advAgency from '../../../img/left-bar/filter/advAgency.svg';
+import advertiser from '../../../img/left-bar/filter/advertiser.svg';
+import brand from '../../../img/left-bar/filter/brand.svg';
+import creator from '../../../img/left-bar/filter/creator.svg';
+import manager from '../../../img/left-bar/filter/manager.svg';
+import section from '../../../img/left-bar/filter/section.svg';
 
-  StyledPanel,
-} from '../../../components/Styles/StyledFilters';
-import { Select, Collapse, DatePicker, Form,Input } from 'antd';
+import { ReactComponent as ArrowDown } from '../../../img/left-bar/filter/arrow_down.svg';
+
+import { FilterMenu, SearchTitle, FilterText, StyledPanel } from '../../../components/Styles/StyledFilters';
+import { Select, Collapse, DatePicker, Form, Input } from 'antd';
 import { BtnGroup, ResetButton, SubmitButton } from '../../../components/Styles/ButtonStyles';
 const { Option } = Select;
 const { Panel } = Collapse;
@@ -25,6 +30,19 @@ const FilterBar = () => {
     form.resetFields();
   };
 
+  const InputIcon = ({ img, alt }) => {
+    return (
+      <span
+        style={{
+          position: 'absolute',
+          transform: 'translate(55% ,30%)',
+          zIndex: '99',
+        }}>
+        <img src={img} alt={alt} />
+      </span>
+    );
+  };
+
   return (
     <FilterMenu
       onKeyDown={(e) => {
@@ -36,46 +54,57 @@ const FilterBar = () => {
       <Form form={form} onFinish={onFinish}>
         <Collapse expandIconPosition={'right'}>
           <StyledPanel header="По дате" key="1">
+            <InputIcon img={date} alt="date icon" />
             <Form.Item name="date">
-            <DatePicker placeholder="01/01/2020" size={'large'} format='DD/MM/YYYY' style={{ width: '100%' }}/>
+              <DatePicker
+                suffixIcon={<ArrowDown />}
+                placeholder="Выберите период"
+                size={'large'}
+                format="DD/MM/YYYY"
+                style={{ width: '100%' }}
+                className="date-picker"
+              />
             </Form.Item>
           </StyledPanel>
           <StyledPanel header="По параметрам" key="2">
-          <Form.Item name="code">
-            <Input placeholder="Код проекта" size={'large'} />
-            </Form.Item>
+              <InputIcon img={brand} alt="brand icon" />
             <Form.Item name="brand">
-              <Select placeholder="Бренд" size={'large'}>
+              <Select placeholder="Бренд" size={'large'} className="placeholder-font">
                 <Option value="case 1">case 1</Option>
                 <Option value="case 2">case 2</Option>
               </Select>
             </Form.Item>
+              <InputIcon img={advertiser} alt="advertiser icon" />
             <Form.Item name="advertiser">
-              <Select placeholder="Рекламодатель" size={'large'}>
+              <Select placeholder="Рекламодатель" size={'large'} className="placeholder-font">
                 <Option value="case 1">case 1</Option>
                 <Option value="case 2">case 2</Option>
               </Select>
             </Form.Item>
+              <InputIcon img={advAgency} alt="advAgency icon" />
             <Form.Item name="advAgency">
-              <Select placeholder="Рекламное агенство " size={'large'}>
+              <Select placeholder="Рекламное агенство " size={'large'} className="placeholder-font">
                 <Option value="case 1">case 1</Option>
                 <Option value="case 2">case 2</Option>
               </Select>
             </Form.Item>
+              <InputIcon img={section} alt="section icon" />
             <Form.Item name="sector">
-              <Select placeholder="Сектор деятельности" size={'large'}>
+              <Select placeholder="Сектор деятельности" size={'large'} className="placeholder-font">
                 <Option value="case 1">case 1</Option>
                 <Option value="case 2">case 2</Option>
               </Select>
             </Form.Item>
+              <InputIcon img={creator} alt="creator icon" />
             <Form.Item name="backOfficeManager">
-              <Select placeholder="Менеджер бэк-офиса" size={'large'}>
+              <Select placeholder="Создатель" size={'large'} className="placeholder-font">
                 <Option value="case 1">case 1</Option>
                 <Option value="case 2">case 2</Option>
               </Select>
             </Form.Item>
+              <InputIcon img={manager} alt="manager icon" />
             <Form.Item name="sellManager">
-              <Select placeholder="Менеджер по продажам" size={'large'}>
+              <Select placeholder="Менеджер по продажам" size={'large'} className="placeholder-font">
                 <Option value="case 1">case 1</Option>
                 <Option value="case 2">case 2</Option>
               </Select>
@@ -92,6 +121,30 @@ const FilterBar = () => {
         .ant-collapse-content{
            background-color: #f5f7fa !important;
         }
+
+        .date-picker>div {
+          margin-left: 25px;
+        }
+
+        .date-picker>div::placeholder {
+          color: #656565;
+        }
+
+        .placeholder-font {
+          font-weight: normal;
+        }
+
+        .placeholder-font>div> .ant-select-selection-placeholder {
+          margin-left: 25px !important;
+        }
+
+        .placeholder-font>div> .ant-select-selection-item {
+          padding-left: 25px !important;
+        }
+
+
+
+
         `}
       </style>
     </FilterMenu>
